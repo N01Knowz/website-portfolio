23eac7b10b536f445a67d5d18d32f74051080617
N01Knowz <ronronarnibal@gmail.com>
2024-06-09 19:31:00 +0800

    Fixed Footer to be mobile responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,20 @@ import { FaPhoneAlt } from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-      <section className="bg-base-300 p-2 text-center md:p-4 lg:p-6" id="contact">
+      <section
+        className="bg-base-300 p-2 text-center md:p-4 lg:p-6"
+        id="contact"
+      >
         <p className="py-2 text-3xl">Contact Me</p>
-        <div className="flex items-center justify-center gap-6 text-xl">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-6 text-xl">
           <p className="flex items-center justify-center gap-2">
             <MdEmail />
-            Email: ronronoarnibal@gmail.com
+            Email: <span> ronronoarnibal@gmail.com</span>
           </p>
-          <p>|</p>
+          <p className="hidden md:block">|</p>
           <p className="flex items-center justify-center gap-2">
             <FaPhoneAlt />
-            Contact Number: 09663393855
+            Contact Number: <span> 09663393855</span>
           </p>
         </div>
       </section>
